2ce5c007351ba0d44e3da623cafcf893c64fd224
Yolanda Ferreira de Souza <75169200+yoleihu@users.noreply.github.com>
2022-06-28 11:59:39 -0300

    inserindo logo e botões

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { AlignTopSimple } from 'phosphor-react';
+import Logo from '../assets/logo-supher.png';
 
 export function Navbar () {
     return(
-        <div>
-            <h1>
-                <AlignTopSimple/>
-                Navbar
-            </h1>
-        </div>
+        <header className="w-full py-6 flex items-center justify-between bg-white">
+            <img src={Logo} width="250px"/>
+            <div>
+                <button>Sobre</button>
+                <button>Aos Hemocentros</button>
+                <button>FAQ</button>
+                <button>Quem somos</button>
+                <button>Registre-se</button>
+                <button>Entrar</button>
+            </div>
+        </header>
     )
 }
