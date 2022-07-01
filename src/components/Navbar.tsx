54703279bb744e8aaa64ad6dc6a32a7b16d79786
Yolanda Ferreira de Souza <75169200+yoleihu@users.noreply.github.com>
2022-06-30 21:42:07 -0300

    chatice

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,27 +2,30 @@ import React from 'react';
 import Logo from '../assets/logo-supher.png';
 import { NavLink, PrimaryButton, SecondaryButton } from './Buttons';
 
-export function Navbar ({
-    link1,
-    link2,
-    link3,
-    link4,
-} : {
-    link1: string;
-    link2: string;
-    link3: string;
-    link4: string;
+interface NavbarProps {
+    links: Array<LinksProps>;
 }
-) {
+
+export type LinksProps = {
+    link: string;
+    label: string;
+}
+
+export function Navbar ({links} : NavbarProps) {
     return(
     <header className="w-full h-32 flex justify-center text-xl font-medium border-b border-red-200 ">
             <div className="w-4/5 flex justify-between items-center">
-                <img src={Logo} width="350px"/>
                 <div>
-                    <NavLink link={link1} label="Sobre"/>
-                    <NavLink link={link2} label="Aos Hemocentros" />
-                    <NavLink link={link3} label="FAQ" />
-                    <NavLink link={link4} label="Quem somos" />
+                <img src={Logo} width="350px"/>
+                    <div>
+                        {links ? (
+                            links?.map((linkProps) => {
+                                <NavLink link={linkProps.link} label={linkProps.label}/>
+                            })
+                        ) : (
+                                <NavLink link={""} label={"linkProps.label"}/>
+                        )}
+                    </div>
                     <SecondaryButton label="Registre-se"/>
                     <PrimaryButton label="Entre"/>
                 </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
-import { Navbar } from "../components/Navbar";
+import { LinksProps, Navbar } from "../components/Navbar";
 
 export function Home() {
+  let ancors: Array<LinksProps>;
+  ancors = [
+    {"link":"frame-sobre", "label":"Sobre"},
+    {"link":"frame-aos-hemocentros", "label":"Aos Hemocentros"},
+    {"link":"frame-faq", "label":"FAQ"},
+    {"link":"frame-quem-somos", "label":"Quem somos"}
+  ]
   return(
     <div className="flex flex-col min-h-screen">
-            <Navbar 
-            link1="frame-sobre" 
-            link2="frame-aos-hemocentros" 
-            link3="frame-faq" 
-            link4="frame-quem-somos"/>
+            <Navbar links={ancors}/>
             <h2 id="frame-sobre">Sobre</h2>
             <h2 id="frame-aos-hemocentros">Aos hemocentros</h2>
             <h2 id="frame-faq">FAQ</h2>
