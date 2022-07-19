f166614db6743bfacd6653fffb49b80bbc8884af
Yolanda Ferreira de Souza <75169200+yoleihu@users.noreply.github.com>
2022-07-19 10:41:04 -0300

    começando frames

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { CheckCircle } from "phosphor-react";
+import { Button } from "../components/Button";
 import { Footer } from "../components/Footer";
 import { LinksProps, Navbar } from "../components/Navbar";
 
@@ -9,12 +11,34 @@ ancors = [
   { "link": "#quem-somos", "label": "Quem somos" }
 ]
 
+export function Check() {
+  return(
+    <CheckCircle size={32} color="#57cba1" weight="fill" />
+  )
+}
+
 export function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <header>
         <Navbar links={ancors} />
       </header>
+
+      <section className="pt-16 lg:pt-24 flex flex-row items-center justify-center" id="welcome">
+        <div className="w-4/5 py-6 ">
+          <div className="w-3/5 ">
+            <h1 className=" text-red-600 text-6xl font-bold ">Faça do seu pet um herói, agende uma consulta!</h1>
+            <p>Você sabia que seu pet pode salvar vidas de outros animais através da doação de sangue?</p>
+            <p>Realize Seu cadastro</p>
+            <p>Inclua seu pet</p>
+            <p>Procure o receptor mais próximo</p>
+            <Button label="Cadastre-se" path='/register' role='primary' />
+          </div>
+          <div>
+            <img src=""/>
+          </div>
+        </div>
+      </section>
 
       <section className="pt-16 lg:pt-24" id="sobre">
         <h2>Sobre</h2>
