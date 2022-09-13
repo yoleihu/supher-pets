e20681b5b4fe6e135d7c2ad8cb0dd90f3cac6811
GiuCM <giuliana.campregher27@gmail.com>
2022-09-13 14:40:53 -0300

    refactor footer

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,24 +1,8 @@
 import { Footer } from "../components/Footer";
 import { UserForm } from "../components/UserForm";
 import { Navbar } from "../components/Navbar";
-import { useEffect, useState } from "react";
 
 export function Register() {
-  const [hasScroolbar, setHasScroolbar] = useState<boolean>();
-
-  useEffect(() => {
-    const updateWindow = () => {
-      if (document.documentElement.offsetHeight + 61 > window.innerHeight) {
-        setHasScroolbar(true);
-      } else {
-        setHasScroolbar(false);
-      }
-    };
-    updateWindow();
-    window.addEventListener("resize", updateWindow);
-    return () => window.removeEventListener("resize", updateWindow);
-  }, []);
-
   return (
     <>
       <header>
@@ -27,11 +11,7 @@ export function Register() {
       <section className="lg:mt-28 mt-24 mb-6">
         <UserForm isRegister />
       </section>
-      <footer
-        className={`w-full ${hasScroolbar ? "static" : "bottom-0 absolute"}`}
-      >
-        <Footer />
-      </footer>
+      <Footer />
     </>
   );
 }
