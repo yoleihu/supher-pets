5f19df6e3725cc7182eaf078614f73eec2f56992
GiuCM <giuliana.campregher27@gmail.com>
2022-09-17 12:20:35 -0300

    enviar email nos forms de contato e recuperar senha

---
+++ src/components/ButtonAsync.tsx
@@ -0,0 +1,19 @@
+import { CircleNotch, SpinnerGap } from "phosphor-react";
+
+export type ButtonAsyncProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  type: string,
+  isLoading?: boolean;
+};
+
+export const ButtonAsync: React.FC<ButtonAsyncProps> = ({ isLoading, type, ...props }) => {
+  return (
+    <>
+      <button type={type} {...props}>
+        {props.children}
+        {isLoading &&
+          <CircleNotch className="animate-spin" />
+        }
+      </button>
+    </>
+  );
+};
