7b6e3b8619f1808b57d378ec4f571a3e8ce5857f
GiuCM <giuliana.campregher27@gmail.com>
2022-10-05 11:35:47 -0300

    modal de adicionar pet

--- src/pages/Guardian.tsx
+++ src/pages/Guardian.tsx
@@ -1,26 +1,31 @@
 import { MagnifyingGlass, PencilSimple, Plus } from "phosphor-react";
-import { useState } from "react";
 import { ButtonNavbar } from "../components/Buttons/ButtonNavbar";
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
-import { Pets, PetsProps } from "../components/Pets";
+import { Pets } from "../components/Pets";
+import { ProfileModal } from "../components/Modals/ProfileModal";
 import { TextField } from "../components/TextField";
-
-const pets: PetsProps[] = [{
-  name: "Totó",
-  image: "/assets/dog-icon.png"
-},
-{
-  name: "Fofinha",
-  image: "/assets/cat-icon.png"
-}]
+import { useState } from 'react'
+import { PetModal } from "../components/Modals/PetModal";
+import { useGuardianInformationStore } from "../store/GuardianInformationStore";
 
 export function Guardian() {
   const [isSearching, setIsSearching] = useState(false);
   const [searchBloodCenter, setSearchBloodCenter] = useState('');
+  const [openEditProfileModal, setOpenEditProfileModal] = useState(false);
+  const [openPetModal, setOpenPetModal] = useState(false);
+  const pets = useGuardianInformationStore();
 
   return (
     <>
+        {openEditProfileModal &&
+          <ProfileModal isGuardian isOpen={openEditProfileModal} onClose={() => { setOpenEditProfileModal(false) }} />
+        }
+
+        {openPetModal &&
+          <PetModal isOpen={openPetModal} onClose={() => { setOpenPetModal(false) }} />
+        }
+
       <Navbar>
         <div className='py-2 items-end bottom-0 flex lg:flex-row flex-col lg:gap-8 w-full lg:w-fit'>
           <ButtonNavbar type="button" label="Sair" path="/" role='secondary' />
@@ -30,26 +35,29 @@ export function Guardian() {
         <div className="w-4/6">
           <div className="flex justify-between items-center">
             <h1 className="md:text-3xl text-xl">Olá Pessoa!</h1>
-            <button className="px-1 py-1 lg:my-0 my-1 w-fit rounded-full">
+            <button onClick={() => setOpenEditProfileModal(true)} className="px-1 py-1 lg:my-0 my-1 w-fit rounded-full">
               <PencilSimple color="#075985" size={24} />
             </button>
           </div>
           <hr className="border-1 border-sky-800 my-4" />
           <h1 className="text-xl">Seus Pets:</h1>
           <div className="flex gap-4 mt-4">
-            {pets && pets.map(pet => (
+            {pets.listPets() && pets.listPets().map(pet => (
               <>
-                <Pets name={pet.name} image={pet.image} />
+                <Pets pet={pet}
+                // name={pet.name} species={pet.species} 
+                // onEditing={() => {setOpenPetModal(true), setIsEditindPet(true)}} 
+                />
               </>
             ))}
-            <button onClick={() => console.log("oi")} className="rounded-full w-20 h-20 bg-red-200 opacity-60 hover:bg-red-200 hover:opacity-80">
+            <button onClick={() => setOpenPetModal(true)} className="rounded-full w-20 h-20 bg-red-200 opacity-60 hover:bg-red-200 hover:opacity-80">
               <Plus className="m-auto" size={34} color="#b91c1c" />
             </button>
           </div>
         </div>
         <div className="w-2/6 bg-white shadow rounded-3xl p-5 min-h-[22rem]">
           {isSearching
-            ? <TextField placeholder="Pesquise aqui" onBlur={() => {!searchBloodCenter && setIsSearching(false)}} autoFocus value={searchBloodCenter} onChange={(value) => setSearchBloodCenter(value)} />
+            ? <TextField placeholder="Pesquise aqui" onBlur={() => { !searchBloodCenter && setIsSearching(false) }} autoFocus value={searchBloodCenter} onChange={(value) => setSearchBloodCenter(value)} />
             : <div className="flex justify-between">
               <h1 className="text-lg font-medium">Hemocentros Próximos</h1>
               <button onClick={() => setIsSearching(true)}>
