a1cc338a2f67403015418a07b6565d7ebf1bd1fa
GiuCM <missiogiulianac@johndeere.com>
2022-11-07 14:34:05 -0300

    mudar icones de cachorro e gato

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { PrincipalRoutes } from './routes/PrincipalRoutes'
 import supherClient from './service/SupherClient'
 import './styles/global.css'
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
