be3aec569b113189d5573c41c2e249f72a5f7ad9
GiuCM <missiogiulianac@johndeere.com>
2022-11-08 22:08:19 -0300

    adicionar tipos sanguíneos

--- src/utils/data/dogsBloodTypes.ts
+++ src/utils/data/dogsBloodTypes.ts
@@ -1,4 +1,10 @@
 export const dogsBloodTypeOptions = [
   { value: '', text: 'Selecione o tipo sanguíneo' },
-  { value: 'abc', text: 'ABC'}
+  { value: 'dea11', text: 'DEA 1.1'},
+  { value: 'dea12', text: 'DEA 1.2'},
+  { value: 'dea13', text: 'DEA 1.3'},
+  { value: 'dea3', text: 'DEA 3'},
+  { value: 'dea4', text: 'DEA 4'},
+  { value: 'dea5', text: 'DEA 5'},
+  { value: 'dea7', text: 'DEA 7'},
 ];
