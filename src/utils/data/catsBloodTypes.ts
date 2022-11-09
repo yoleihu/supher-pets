74f8b8e9967b17cfb73c0335aabaa2c24bbf1612
GiuCM <missiogiulianac@johndeere.com>
2022-11-08 22:08:19 -0300

    adicionar tipos sanguíneos

--- src/utils/data/catsBloodTypes.ts
+++ src/utils/data/catsBloodTypes.ts
@@ -1,3 +1,6 @@
 export const catsBloodTypeOptions = [
   { value: '', text: 'Selecione o tipo sanguíneo' },
+  { value: 'a', text: 'A'},
+  { value: 'b', text: 'B'},
+  { value: 'ab', text: 'AB'},
 ];
